3fa9138d8ee8e3b29627912185cae858463b101c
Olaf Klischat <olaf.klischat@gmail.com>
2004-03-31 01:53:51 +0000

    Simple template expanding tests

---
+++ perl/test/Template.pm
@@ -0,0 +1,66 @@
+package Template;
+
+use strict;
+use Carp 'croak';
+
+sub new {
+    my $self = bless {}, shift;
+    $self->load(shift) if @_;
+    $self;
+}
+
+
+sub load {
+    my ($self, $filename) = @_;
+    local $/ = "\n";
+    open(F, "<$filename") or croak "couldn't open $filename: $!";
+    my $templ = "";
+    while (<F>) {
+        my @line = split /<<<([^=].*?)>>>/;
+        for (my ($printed, $code) = (shift @line, shift @line);
+             defined $printed;
+             ($printed, $code) = (shift @line, shift @line)) {
+
+            $templ .= q[$self->print ( ];
+            my @printed = split(/<<<=(.*?)>>>/, $printed);
+            for (my ($text, $expr) = (shift @printed, shift @printed);
+                 defined $text;
+                 ($text, $expr) = (shift @printed, shift @printed)) {
+                $templ .= qq[q($text) . ];
+                $templ .= "\$self->quote_expr(scalar($expr)) ." if $expr;
+            }
+            $templ .= q["");];
+            $templ .= $code if $code;
+        }
+    }
+    $self->{templ} = $templ;
+}
+
+
+# override to print to string, file, ... instead of stdout
+# and/or apply transformations to the expanded text
+sub print {
+    my ($self, $string) = @_;
+    print $string;  # just print to stdout by default
+}
+
+# override to apply some transformation to user expressions
+# (e.g. quote HTML/XML metacharacters etc.)
+sub quote_expr {
+    my ($self, $expr) = @_;
+    $expr;  # return the expression without any transformations by default
+}
+
+
+sub run {
+    no strict 'vars';
+    my ($self) = @_;
+    my $callingpkg = caller();
+    eval qq[package $callingpkg; $self->{templ}];
+    croak $@ if $@;
+}
+
+
+1;
+
+# TODO: escaping...
---
+++ perl/test/TemplateTest.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+
+use Template;
+
+
+my $rs = Template->new("rundschreiben.templ");
+
+our @people =
+( {name=>'Meier', sex=>'m', debt=>4000},
+  {name=>'Schubert', sex=>'f', debt=>2500},
+  {name=>'Hubertus', sex=>'m', debt=>3000}
+);
+
+$rs->run();
