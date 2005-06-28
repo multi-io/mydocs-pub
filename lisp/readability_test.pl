21ac7688e357a0752f22c777eff2ec418bea59bc
Olaf Klischat <olaf.klischat@gmail.com>
2005-06-28 04:31:28 +0000

    perl test

---
+++ lisp/readability_test.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+#%match_window_profile = (
+#   [WM_CLASS, "Term"] => $VAR1={ignore-program-position=>1, foo=>42},
+#   [WND_TITLE, "MyApp"] => $VAR1,
+#   [WM_CLASS, "Lalala"] => {ignore-program-position=>0},
+#   [WND_TITLE, "Netscape"] => {sticky=>1, cyclable=>0},
+#);
+
+%match_window_profile = ();
+
+sub add_window_matcher {
+    my ($prop, $value, %actions) = @_;
+    foreach my $k (keys %actions) {
+        $match_window_profile{[$prop,$value]}->{$k} = $actions{$k};
+    };
+}
+
+
+$match_window_profile{[WM_CLASS, "Term"]} = $val1 = {ignore_program_position=>1, foo=>42};
+$match_window_profile{[WND_TITLE, "MyApp"]} = $val1;
+$match_window_profile{[WM_CLASS, "Lalala"]} = {ignore_program_position=>0};
+$match_window_profile{[WND_TITLE, "Netscape"]} = {sticky=>1, cyclable=>0};
+
+use Data::Dumper;
+
+print ">>BEFORE:\n";
+print Dumper(%match_window_profile);
+
+add_window_matcher(WND_TITLE, "Netscape", sticky=>5, olaf=>"klischat");
+
+
+print "\n\n>>AFTER:\n";
+print Dumper(%match_window_profile);
+
+
+
+## doesn't work -- arrays as hash keys don't work...
