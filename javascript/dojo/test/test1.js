f31d6cabfc3812b56844c9e0d58818a17b450d51
Olaf Klischat <olaf.klischat@gmail.com>
2011-03-06 20:14:27 +0100

    dojo tests: dynamic widget creation & manipulation

--- javascript/dojo/test/test1.js
+++ javascript/dojo/test/test1.js
@@ -10,6 +10,20 @@ dojo.addOnLoad(
                                 });
         var dlg = dijit.byId("mydialog");
         dojo.connect(dijit.byId("showDlgBtn"), "onClick", function() {
-                     dlg.show();
+                         dlg.show();
+                     });
+        var dynButtons = [];
+        dojo.connect(dijit.byId("createBtnBtn"), "onClick", function() {
+                         var btn = new dijit.form.Button({label:"Say Hello"});
+                         dojo.place(btn.domNode, dojo.query("h1")[0], "after");
+                         dojo.connect(btn, "onClick", function() {
+                                          alert("Hello World!");
+                                      });
+                         dynButtons.push(btn);
+                     });
+        dojo.connect(dijit.byId("colorBtn"), "onClick", function() {
+                         dojo.forEach(dynButtons, function(btn) {
+                                          btn.attr("style", "color: green");
+                                      });
                      });
     });
