97551132856b53c13cb29fcccc305606eec33738
Olaf Klischat <olaf.klischat@gmail.com>
2012-02-21 20:00:30 +0100

    more gradle tests: java, multi-project

---
+++ gradle/test/multiprj/bla/calc/src/main/java/oklischat/gradle/multiprjtest/calc/Calculator.java
@@ -0,0 +1,7 @@
+package oklischat.gradle.multiprjtest.calc;
+
+public class Calculator {
+    public static int add(int x, int y) {
+        return x+y;
+    }
+}
---
+++ gradle/test/multiprj/common/src/main/java/oklischat/gradle/multiprjtest/common/WiseCracker.java
@@ -0,0 +1,10 @@
+package oklischat.gradle.multiprjtest.common;
+
+import oklischat.gradle.multiprjtest.calc.Calculator;
+
+public class WiseCracker {
+
+    public static String getWisdom() {
+        return "23+19=" + Calculator.add(23,19);
+    }
+}
