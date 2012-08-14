e186172c5121c0a48d293b160f5f823bc2093ff3
Olaf Klischat <olaf.klischat@gmail.com>
2012-08-14 03:08:41 +0200

    OpenSSL CS creation, certificate creation & signing

--- pki/test/openssl/gen_cert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-openssl req -new -x509 -key privkey.pem -out cacert.pem -days 1095
--- pki/test/openssl/gen_privkey.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#openssl genrsa -des3 -out privkey.pem 2048  # with -des3 the key will be passphrase-encrypted
-
-openssl genrsa -out privkey.pem 2048
