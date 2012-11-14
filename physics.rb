0c2e801609279c109da7baf3579d08047f94ef54
Olaf Klischat <olaf.klischat@gmail.com>
2012-11-14 02:14:42 +0100

    physics.rb additions

--- physics.rb
+++ physics.rb
@@ -25,7 +25,6 @@ mm=1e-3
 um=1e-6
 nm=1e-9
 km=1000.0
-ly=c*year
 au=149.6e6*km
 pc=au/arcsec
 
@@ -35,15 +34,30 @@ r_sun=1.392e6*km/2
 
 rrad_sun=r_sun/au
 
+# volumes. base unit: m^3
+m3=meter**3
+litre=(10*cm)**3
+barrel=119.240471*litre
+
 # velocities. base unit: meters/sec
 m_per_s=meter/sec
 kmh=km/hr
 c=299792458.0
 
+# distance
+ly=c*year
+
+
+# mass, base unit: kg
+kg=1.0
+gram=0.001*kg
+ton=1000.0*kg
+
 
 # energies, base unit: joule
 j=1.0
 ws=j
+mj=1e6*j
 wh=3600.0*ws
 kwh=1000.0*wh
 mwh=1000.0*kwh
