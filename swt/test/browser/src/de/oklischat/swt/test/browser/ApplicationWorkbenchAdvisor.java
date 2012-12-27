71a61f21fa59364c454dc4a57cb344519364a067
Olaf Klischat <olaf.klischat@gmail.com>
2012-12-27 06:02:06 +0100

    SWT browser test

---
+++ swt/test/browser/src/de/oklischat/swt/test/browser/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package de.oklischat.swt.test.browser;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "de.oklischat.swt.test.browser.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
