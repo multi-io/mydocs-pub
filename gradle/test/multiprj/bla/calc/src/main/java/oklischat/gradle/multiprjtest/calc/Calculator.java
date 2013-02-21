ae4fc2ba6475adb980f58f7c66207045f8229694
Olaf Klischat <olaf.klischat@gmail.com>
2013-02-21 01:50:48 +0100

    gradle: multiprj test: test using dependencies of dependencies

--- gradle/test/multiprj/bla/calc/src/main/java/oklischat/gradle/multiprjtest/calc/Calculator.java
+++
@@ -1,7 +0,0 @@
-package oklischat.gradle.multiprjtest.calc;
-
-public class Calculator {
-    public static int add(int x, int y) {
-        return x+y;
-    }
-}
--- gradle/test/multiprj/hw/src/main/java/oklischat/gradle/HW.java
+++ gradle/test/multiprj/hw/src/main/java/oklischat/gradle/HW.java
@@ -2,12 +2,16 @@ package oklischat.gradle;
 
 import oklischat.gradle.multiprjtest.common.WiseCracker;
 import oklischat.gradle.multiprjtest.calc.Calculator;
+import org.apache.log4j.Logger;
 
 public class HW {
+
+    static final Logger log = Logger.getLogger(HW.class);
 
     public static void main(String[] args) throws Exception {
         System.out.println("HW! " + WiseCracker.getWisdom());
         System.out.println("  direct calc: 1+1=" + Calculator.add(1,1));
+        log.info("direct log4j (which is a dependency of calc)");
     }
 
 }
