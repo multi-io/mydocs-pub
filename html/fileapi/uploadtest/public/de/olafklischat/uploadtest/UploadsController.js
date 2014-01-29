0f1d81e6ae05aba4132fcefb376f4a2d4f1e3c37
Olaf Klischat <olaf.klischat@gmail.com>
2014-01-29 03:51:01 +0100

    uploadtest: UploadsController widget

--- html/fileapi/uploadtest/public/de/olafklischat/uploadtest/UploadsController.js
+++ html/fileapi/uploadtest/public/de/olafklischat/uploadtest/UploadsController.js
@@ -1,20 +1,31 @@
 dojo.provide('de.olafklischat.uploadtest.UploadsController');
 
+dojo.require("dijit._Widget");
+dojo.require("dijit._Templated");
+dojo.require('dojo.NodeList-manipulate');
+dojo.require("dojox.html.entities");
+dojo.require("dijit.ProgressBar");
 
-dojo.declare("de.olafklischat.uploadtest.UploadsController", null, {
+dojo.declare("de.olafklischat.uploadtest.UploadsController", [dijit._Widget, dijit._Templated], {
 
-    constructor: function(containerId) {
+    templateString: '<div style="overflow:scroll"><table dojoAttachPoint="_mainTable"></table></div>',
+
+    constructor: function() {
         var self = this;
-        self._container = dojo.byId(containerId);
-
-        dojo.when(dojo.xhrGet({url:"uploads/all_required", handleAs:"json"}), function(uploads) {
+        dojo.when(dojo.xhrGet({url:"uploads/all_existing", handleAs:"json"}), function(uploads) {
             dojo.forEach(uploads, function (ul) {
                 console.log(ul.file);
-                dojo.create("div", {innerHTML: ul.file || ul.name}, self._container);
+                var tr = dojo.create("tr", null, self._mainTable);
+                var td1 = dojo.create("td", {innerHTML: dojox.html.entities.encode(ul.file || ul.name)}, tr);
+                var td2 = dojo.create("td", null, tr);
+                var td3 = dojo.create("td", {innerHTML: '<button>X</button>'}, tr);
+                var pb = new dijit.ProgressBar({style: "width: 300px"});
+                pb.placeAt(td2);
+                pb.set('value', 42);
             });
         },
         function(error) {
-            self._container.innerHTML = ('ERROR: ' + error);
+            console.log("ERROR: " + error);
         });
     }
 });
