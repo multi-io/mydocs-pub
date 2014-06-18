2edb6a704b4d3e2cb70b20328143b3e7c9f6e160
Olaf Klischat <olaf.klischat@gmail.com>
2014-06-19 00:46:30 +0200

    WIP

--- javascript/dojo/dialog/de/olafklischat/ConfirmDialog.js
+++ javascript/dojo/dialog/de/olafklischat/ConfirmDialog.js
@@ -4,14 +4,21 @@ dojo.require("dijit._Templated");
 
 dojo.declare('de.olafklischat.ConfirmDialog', [dijit.Dialog, dijit._Templated], {
 
-		title: 'Confirm',
-        templateString: dojo.cache("de.olafklischat", "ConfirmDialog.html"),
-        widgetsInTemplate: true,
+    title: 'Confirm',
+    templateString: dojo.cache("de.olafklischat", "ConfirmDialog.html"),
+    widgetsInTemplate: true,
 
-		constructor: function(options){
-			if (options.message) {
-				this.content = options.message;
-			}
-		}
+    constructor: function(options){
+        var self = this;
+        self.content = options.message;
+    }
 
 });
+
+
+//TODO avoid polluting the global namespace
+confirm = function(msg, buttons) {
+    var dialog = new de.olafklischat.ConfirmDialog({message:msg}); //TODO: only have one instance, at least in the DOM
+    window.dlg = dialog; //debugging
+    dialog.show();
+}
