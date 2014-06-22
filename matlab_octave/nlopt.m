67a7a95d235ec834703d7e70a2415b096f4b1876
Olaf Klischat <olaf.klischat@gmail.com>
2014-06-22 19:20:27 +0200

    R2->R plotting -- variant 3

--- matlab_octave/nlopt.m
+++ matlab_octave/nlopt.m
@@ -5,7 +5,7 @@
 
 1;
 
-global M = [3 2]
+global M = [3;2]
 
 function phi = phi(x)
   global M
@@ -15,26 +15,24 @@ endfunction
 
 ##plot it -- incredibly convoluted
 
-
 # nested function -- only works when copied & pasted into the command line???
-function plotR2_R = plotR2_R(func, xrange, yrange)
-
-  function funcmesh = funcmesh(x, y)
-    funcmesh = ones([size(x,1), size(x,2)]);
-    for m = 1:size(x,1)
-      for n = 1:size(x,2)
-        funcmesh(m,n) = func([x(m,n), y(m,n)]);
-      endfor
-    endfor
-  endfunction
-
+function plotR2_R = plotR2_R(func, domain)
   #ezmesh(funcmesh, [0 5])  #error
   #ezmesh('funcmesh', [0 5])  #error
-  #ezmesh(@funcmesh, xrange, yrange) #error ("handles to nested functions are not yet supported")
-  ezmesh(@(x,y) funcmesh(x,y), xrange, yrange)
+  #ezmesh(@funcmesh, domain) #error ("handles to nested functions are not yet supported")
+  ezmesh(@(x,y) funcmesh(func, x, y), domain)
+endfunction
 
+# private to plotR2_R -- nested only works on command line??
+function funcmesh = funcmesh(func, xs, ys)
+  funcmesh = ones([size(xs,1), size(xs,2)]);
+  for m = 1:size(xs,1)
+    for n = 1:size(xs,2)
+      funcmesh(m,n) = func([xs(m,n); ys(m,n)]);
+    endfor
+  endfor
 endfunction
 
 
-plotR2_R(@phi, [0 5], [0 5])  #error
+plotR2_R(@phi, [0 5 0 5])
 
