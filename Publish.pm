a115a52522c110c3a07f25e6bb2fa8a23f011fe7
Olaf Klischat <olaf.klischat@gmail.com>
2014-08-24 15:53:17 +0200

    wwwpublish split up

---
+++ Publish.pm
@@ -0,0 +1,122 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+BEGIN { @INC = ("perl/test", @INC); }
+
+use Template;
+use File::Copy;
+
+sub htmlizable {
+    local ($_) = shift;
+    m!bsh$! or
+    m!c$! or
+    m!cc$! or
+    m!cgi$! or
+    m!classpath$! or
+    m!consign$! or
+    m!cs$! or
+    m!css$! or
+    m!dat$! or
+    m!dtd$! or
+    m!edit$! or
+    m!el$! or
+    m!eli$! or
+    m!env$! or
+    m!gplot$! or
+    m!h$! or
+    m!jad$! or
+    m!java$! or
+    m!jpr$! or
+    m!js$! or
+    m!latex$! or
+    m!li$! or
+    m!local$! or
+    m!log$! or
+    m!\.ml$! or
+    m!pac$! or
+    m!php3$! or
+    m!pl$! or
+    m!pm$! or
+    m!policy$! or
+    m!project$! or
+    m!py$! or
+    m!rb$! or
+    m!sax2$! or
+    m!show$! or
+    m!sql$! or
+    m!templ$! or
+    m!tex$! or
+    m!txt$! or
+    m!txt,v$! or
+    m!url$! or
+    m!xerces$! or
+    m!Makefile$! or
+    m!xml$!;
+}
+
+sub publish($$);
+
+sub publish($$) {
+    # "our" variables are "our" so the template can acces it
+    # we use "my" copies of those variables in this routine because
+    #    using "our" (i.e. dynamically scoped) variables in recursive
+    #    functions tends to be messy
+    my ($_dir, $targetdir) = @_;
+    print STDERR "publishing: $targetdir\n";
+    unless (-d $targetdir) {
+        mkdir $targetdir or die "couldn't mkdir $targetdir: $!";
+    }
+    open(IDXHTM, ">$targetdir/index.html");
+    my $_target_templ = "wwwpublish.d/index.templ";
+    my $tpl = Template->new("wwwpublish.d/wrapper.templ", *IDXHTM);
+
+    our $dir = $_dir;
+    our $target_templ = $_target_templ;
+    $tpl->run();
+
+    close IDXHTM;
+
+    foreach my $_f (@{$_dir->{FILES}}) {
+        my $_fqsrcname = "$_dir->{PATH}$_f";
+
+        if (!htmlizable($_fqsrcname)) {
+            copy($_fqsrcname, "${targetdir}/$_f");
+            next;
+        }
+
+        local $/=undef;
+        open(F,"<$_fqsrcname") or die "couldn't open $_fqsrcname: $!";
+        my $_text = <F>;
+        close F;
+        $target_templ = "wwwpublish.d/file.templ";
+        open(FILEHTM, ">${targetdir}/$_f.html");
+        my $tpl = Template->new("wwwpublish.d/wrapper.templ", *FILEHTM);
+
+        our $f = $_f;
+        our $fqsrcname = $_fqsrcname;
+        our $text = $_text;
+        $tpl->run();
+
+        close FILEHTM;
+    }
+
+    foreach my $d (@{$dir->{DIRS}}) {
+        publish($d, "$targetdir/$d->{NAME}");
+    }
+}
+
+
+# for use by templates
+sub htmlescape {
+    $_=shift;
+    my $repl = rand() . rand() . rand();
+    my $replre = qr/$repl/;
+    s!&!$repl!sig;
+    s!<!&lt;!sig;
+    s!>!&gt;!sig;
+    s!$replre!&amp;!sig;
+    $_;
+}
+
+1;
