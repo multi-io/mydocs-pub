953d65ef7b0e2cb402e5dbcb9e13ab90d316f3a4
Olaf Klischat <o.klischat@syseleven.de>
2016-11-25 20:59:40 +0100

    upgrade to 1f82c851cd92287e50368eef0524198cd461b68d

--- Publish.pm
+++ Publish.pm
@@ -6,6 +6,8 @@ BEGIN { @INC = ("perl/test", @INC); }
 
 use Template;
 use File::Copy;
+use File::Basename;
+use File::Temp qw/ tempfile /;
 
 sub htmlizable {
     local ($_) = shift;
@@ -34,6 +36,7 @@ sub htmlizable {
     m!\.launch$! or
     m!\.local$! or
     m!\.log$! or
+    m!\.md$! or
     m!\.ml$! or
     m!\.m$! or
     m!\.pac$! or
@@ -87,27 +90,66 @@ sub publish($$) {
         my $_fqsrcname = "$_dir->{PATH}$_f";
 
         copy($_fqsrcname, "${targetdir}/$_f");
-        next unless (htmlizable($_fqsrcname));
-
-        local $/=undef;
-        open(F,"<$_fqsrcname") or die "couldn't open $_fqsrcname: $!";
-        my $_text = <F>;
-        close F;
-        $target_templ = "wwwpublish.d/file.templ";
-        open(FILEHTM, ">${targetdir}/$_f.html");
-        my $tpl = Template->new("wwwpublish.d/wrapper.templ", *FILEHTM);
-
-        our $f = $_f;
-        our $fqsrcname = $_fqsrcname;
-        our $text = $_text;
-        $tpl->run();
-
-        close FILEHTM;
+        
+        if (htmlizable($_fqsrcname)) {
+            htmlize($_fqsrcname, "${targetdir}/$_f.html");
+        }
     }
 
     foreach my $d (@{$dir->{DIRS}}) {
         publish($d, "$targetdir/$d->{NAME}");
     }
+}
+
+
+sub htmlize($$) {
+    my ($srcname, $destname) = @_;
+    my ($ext) = basename($srcname) =~ /(\.[^.]+)$/;
+    $ext = '' unless $ext;
+
+    for ($ext) {
+        /\.md$/ and do {
+
+            # markdown to html
+
+            open(SRC, "<$srcname") or die "couldn't open $srcname: $!";
+            local $/=undef;
+            our $text = <SRC>;  # for file_md.templ below
+            close SRC;
+
+            our $f = basename($destname, '.html');
+            our $fqsrcname = $srcname;
+
+            my ($full_md_fd, $full_md_fn) = tempfile();
+            my $tpl = Template->new("wwwpublish.d/file_md.templ", *$full_md_fd);
+            $tpl->run();
+            
+            system('pandoc', $full_md_fn, '-o', $destname) == 0 or die "pandoc $full_md_fn -o $destname failed: $?";
+
+            unlink $full_md_fn;
+            
+            last;
+        };
+
+        # fallback -- generic text to html
+
+        open(SRC, "<$srcname") or die "couldn't open $srcname: $!";
+        local $/=undef;
+        my $_text = <SRC>;
+        close SRC;
+        
+        our $target_templ = "wwwpublish.d/file.templ";
+        open(DEST, ">$destname");
+        my $tpl = Template->new("wwwpublish.d/wrapper.templ", *DEST);
+
+        our $f = basename($destname, '.html');
+        our $fqsrcname = $srcname;
+        our $text = $_text;
+        $tpl->run();
+
+        close DEST;
+    }
+
 }
 
 
