cfdd4ca47261d419011adc5c99b10b3c9807ed3c
Olaf Klischat <olaf.klischat@gmail.com>
2026-05-30 15:17:30 +0200

    math/leapseconds: past leap seconds data

---
+++ math/leapseconds/recorded/__init__.py
@@ -0,0 +1,1 @@
+from .lod_deltas import get_past_lod_deltas
---
+++ math/leapseconds/recorded/lod_deltas.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from importlib import resources
+
+
+def get_past_lod_deltas(
+    filename="./eopc04_IAU2000.62-now",
+):
+    dates = []
+    lods = []
+
+    with resources.files(__package__).joinpath(
+        filename
+    ).open("r", encoding="utf-8") as file:
+        for line in file:
+            if not line or line.startswith("#") or line.startswith(" ") or line.startswith("\t"):
+                continue
+
+            parts = line.split()
+            if len(parts) < 8:
+                continue
+
+            year = int(parts[0])
+            month = int(parts[1])
+            day = int(parts[2])
+            lod = float(parts[7])
+
+            dates.append(datetime(year, month, day))
+            lods.append(lod)
+
+    return dates, lods
